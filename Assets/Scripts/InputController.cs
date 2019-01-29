85279d2512b321bb499b32015d9a222ab5e2708c
Javier Riera Chirivella <mrvizious@gmail.com>
2019-01-30 00:02:35 +0100

    Trying VS Code and stuff
    This is just a "just-in-case" commit

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+
+	private bool doingActions;
+    
+	private void Start() {
+
+		doingActions = false;
+
+	}
+
+	public void Update() {
+		if (Input.anyKeyDown && !doingActions) {
+
+			string inputKey = Input.inputString;
+
+			if (inputKey.Length == 1 && (ValidKey(inputKey) || inputKey.Equals(" "))) {
+				//TODO: Add letter
+			}
+
+		}
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			//TODO: Start Actions Routine
+		}
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			//TODO: Pause menu
+		}
+
+	}
+
+	private bool ValidKey(string s) {
+		return Regex.IsMatch(s, @"^[a-z]+$");
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+[RequireComponent(typeof(Transform))]
+public class PlayerController : MonoBehaviour {
 
-    // This function receives a List of strings with the names of the actions to do, and creates a Coroutine that performs the movements one by one
-    private void PerformListOfActions() {
-        
-    }
+	private void PerformActions(List<string> actions) {
 
-    private void PerformAction() {
-        
-    }
+	}
+
+
+	private void PerformAction() {
+
+	}
 }
