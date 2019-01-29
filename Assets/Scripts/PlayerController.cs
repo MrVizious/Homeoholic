72a5ab58890b29953d999453d1e51894ab83c330
Javier Riera Chirivella <mrvizious@gmail.com>
2019-01-29 02:50:45 +0100

    Small Scene created and VS Code style formatter configured

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // This function receives a List of strings with the names of the actions to do, and creates a Coroutine that performs the movements one by one
+    private void PerformListOfActions() {
+        
+    }
+
+    private void PerformAction() {
+        
+    }
+}
