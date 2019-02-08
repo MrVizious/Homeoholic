e207d5ad1a7c366fc8c020a256ad1b918c618d8f
Javier Riera Chirivella <mrvizious@gmail.com>
2019-02-08 22:27:51 +0100

    Pause Menu Implemented
    Thanks, @Brackeys, you rule!

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class PauseMenuController : MonoBehaviour {
 	private bool isPaused = false;
 
+	public bool getIspaused() {
+		return this.isPaused;
+	}
+
+	[SerializeField]
+	private GameObject PauseMenu;
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
-			isPaused = !isPaused;
+			if (isPaused) {
+				ResumeGame();
+			} else {
+				PauseGame();
+			}
 		}
 	}
+
+	public void PauseGame() {
+		Time.timeScale = 0f;
+		PauseMenu.SetActive(true);
+		isPaused = true;
+
+	}
+	public void ResumeGame() {
+		Time.timeScale = 1f;
+		PauseMenu.SetActive(false);
+		isPaused = false;
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
 }
